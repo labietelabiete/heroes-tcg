ae460a390cb53ca3c2d6d32757d988a4c82e16c6
labietelabiete <victorhugogote@gmail.com>
2026-01-30 17:54:20 +0100

    htcg-002: Implement HttpClient class with GET, POST, and PUT methods for API interactions

---
+++ app/modules/shared/HttpClient/HttpClient.ts
@@ -0,0 +1,72 @@
+import { getConfigs } from '@/app/config'
+
+const { API_CONFIG } = getConfigs()
+
+export class HttpClient {
+    static async get<T>(endpoint: string, options?: RequestInit): Promise<T> {
+        const url = `${API_CONFIG.URL}${endpoint}`
+        const response = await fetch(url, {
+            ...options,
+            headers: {
+                ...options?.headers,
+                'Access-Control-Allow-Origin': '*',
+                Authorization: `Bearer ${API_CONFIG.KEY}`
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        return response.json() as Promise<T>
+    }
+
+    static async post<T>(endpoint: string, body: unknown, options?: RequestInit): Promise<T> {
+        const url = `${API_CONFIG.URL}${endpoint}`
+        const headers: HeadersInit = {
+            'Access-Control-Allow-Origin': '*',
+            Authorization: `Bearer ${API_CONFIG.KEY}`,
+            ...options?.headers
+        }
+
+        let processedBody = body
+        if (!(body instanceof FormData)) {
+            processedBody = JSON.stringify(body)
+            headers['Content-Type'] = 'application/json'
+        }
+
+        const response = await fetch(url, {
+            method: 'POST',
+            body: processedBody,
+            ...options,
+            headers
+        })
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        return response.json() as Promise<T>
+    }
+
+    static async put<T>(endpoint: string, body: unknown, options?: RequestInit): Promise<T> {
+        const url = `${API_CONFIG.URL}${endpoint}`
+        const response = await fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify(body),
+            ...options,
+            headers: {
+                'Content-Type': 'application/json',
+                ...options?.headers,
+                'Access-Control-Allow-Origin': '*',
+                Authorization: `Bearer ${API_CONFIG.KEY}`
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        return response.json() as Promise<T>
+    }
+}
